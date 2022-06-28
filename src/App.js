3c10a4cd5eaeaf267bb567cb69abb908a66fd050
GaliNeufeld <101136202+GaliNeufeld@users.noreply.github.com>
2022-06-28 18:59:31 -0400

    Added a keyword search form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import owl from "./owl.png";
+import Dictionary from "./Dictionary";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={owl} className="owl-image img-fluid" alt="owl" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={owl} className="owl-image img-fluid" alt="owl" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Gali Neufeld</small>
+        </footer>
+      </div>
     </div>
   );
 }
