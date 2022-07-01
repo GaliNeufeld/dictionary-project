8200d509ece5752fb2964cd184f6d1638781cbb3
GaliNeufeld <101136202+GaliNeufeld@users.noreply.github.com>
2022-07-01 19:21:39 -0400

    Added Fontawesome icon

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,14 +1,15 @@
 import React from "react";
 
+
 export default function Phonetic(props) {
     console.log(props.phonetic);
     return (
-        <div className="Phonetic">
-           < a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-            Listen {" "}
-           </a> 
-           <br />
-           {props.phonetic.text}
-        </div>
-    )
+      <div className="Phonetic">
+        <a href={props.phonetic.audio} target="_blank" rel="noreferrer noopener">
+          <i class="fa-solid fa-volume-high"></i>
+        </a>
+        <br />
+        {props.phonetic.text}
+      </div>
+    );
 } 
