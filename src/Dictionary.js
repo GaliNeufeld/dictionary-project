b1a2f3c29a072b955dcfbdbb6a12ef07b6779dbd
GaliNeufeld <101136202+GaliNeufeld@users.noreply.github.com>
2023-12-01 20:32:21 -0500

    reduced # of photos to 3

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -25,7 +25,7 @@ export default function Dictionary(props) {
     }
 
     function search() {
-      let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+      let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}&per_page=3`;
       axios.get(apiUrl).then(handleDictionaryResponse);   
 
       let apiKey = "303b041t9dc7c1ce08f4ao48696a7fa8";
