977c0e24fcd3774f9a38f78855b04c688db89c08
GaliNeufeld <101136202+GaliNeufeld@users.noreply.github.com>
2023-12-02 18:32:40 -0500

    corrected an error on json and styled images

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,65 +4,58 @@ import Results from "./Results";
 import Photos from "./Photos";
 import "./Dictionary.css";
 
-
-
-
 export default function Dictionary(props) {
-    let [keyword, setKeyword] = useState(props.defaultKeyword);
-    let [results, setResults] = useState(null);
-    let [loaded, setLoaded] = useState(false);
-    let [photos, setPhotos] = useState(null);
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
+  let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-    
-    
-    function handleKeywordChange(event) {
-        setKeyword(event.target.value);
-    }
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        search();
-    }
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
 
-    function search() {
-      let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-      axios.get(apiUrl).then(handleDictionaryResponse);   
+  function search() {
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleDictionaryResponse);
 
-      let apiKey = "303b041t9dc7c1ce08f4ao48696a7fa8";
-      let pexelsApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${apiKey}&per_page=3`;
-      
-      axios.get(pexelsApiUrl).then(handleImagesResponse);
-    }
+    let apiKey = "303b041t9dc7c1ce08f4ao48696a7fa8";
+    let pexelsApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${apiKey}`;
 
-    function handleDictionaryResponse(response) {
-        setResults(response.data[0]);
-        setLoaded(true);
-    }
+    axios.get(pexelsApiUrl).then(handleImagesResponse);
+  }
 
-    function handleImagesResponse(response) {
-        setPhotos(response.data.photos);
-    }
+  function handleDictionaryResponse(response) {
+    setResults(response.data[0]);
+    setLoaded(true);
+  }
 
-    if (loaded===true) {
-      return (
-        <div className="Dictionary">
-          <section>
-            <form onSubmit={handleSubmit}>
-              <input
-                type="search"
-                onChange={handleKeywordChange}
-               placeHolder="Please type a word..."
-                
-              />
-            </form>
-          </section>
-          <Results results={results} /> 
-          <Photos photos={photos} />
-        </div>
-      );
-    } else {
-        search();
-        return "Loading...";
-    } 
+  function handleImagesResponse(response) {
+    setPhotos(response.data.photos);
+  }
 
-}      
+  if (loaded === true) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="search"
+              onChange={handleKeywordChange}
+              placeHolder="Please type a word..."
+            />
+          </form>
+        </section>
+        <Results results={results} />
+        <Photos photos={photos} />
+      </div>
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
+}
